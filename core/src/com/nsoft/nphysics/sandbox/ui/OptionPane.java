13e2610596df9a200cba72b08438774143f64d88
DavidNexuss <dgtgar01@gmail.com>
2018-06-30 16:44:25 +0200

    Initial UI commit

---
+++ core/src/com/nsoft/nphysics/sandbox/ui/OptionPane.java
@@ -0,0 +1,7 @@
+package com.nsoft.nphysics.sandbox.ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class OptionPane extends Table {
+
+}
---
+++ core/src/com/nsoft/nphysics/sandbox/ui/UIStage.java
@@ -0,0 +1,8 @@
+package com.nsoft.nphysics.sandbox.ui;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class UIStage extends Stage{
+
+	
+}
